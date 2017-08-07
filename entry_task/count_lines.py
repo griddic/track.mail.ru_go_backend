e4b5e65c61d6951f27b20384d85cac9dd060c8d7
Grigory Koksharov <griddic@mail.ru>
2017-08-07 22:29:18 +0300

    realization + tests

--- entry_task/count_lines.py
+++ entry_task/count_lines.py
@@ -2,6 +2,10 @@ import argparse
 
 import os
 
+import re
+
+DO_NOT_WANT_TO_LOAD_IN_MEMORY_LIMIT = 10 ** 7 #bytes
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -9,8 +13,21 @@ def parse_args():
     return parser.parse_args()
 
 
+def count_lines_in_file(path, buffer=DO_NOT_WANT_TO_LOAD_IN_MEMORY_LIMIT):
+    lines_counted = 1
+    with open(path) as inn:
+        while True:
+            content = inn.read(DO_NOT_WANT_TO_LOAD_IN_MEMORY_LIMIT)
+            if len(content) == 0:
+                break
+            lines_counted += len(re.findall(r"\n", content))
+    return lines_counted
+
 if __name__ == "__main__":
     args = parse_args()
     assert os.path.exists(args.file), "No file '%s' found" % args.file
     assert not os.path.isdir(args.file), "Please specify path to a file, not to the directory."
+
+    print(count_lines_in_file(args.file))
+
 
---
+++ entry_task/tests/test_lines_couter.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+
+from entry_task.count_lines import count_lines_in_file
+
+
+@pytest.mark.parametrize(("file_path", 'lines_amount'), [
+    ("1.txt", 1),
+    ("10.txt", 10),
+    ("empty_tail.txt", 10),
+    ("empty_head.txt", 10),
+    ("100.txt", 100),
+    ("666.txt", 666)
+])
+@pytest.mark.parametrize("buffer", [1,5, 7, 100, None])
+def test_counter(file_path, lines_amount, buffer):
+    file_path = os.path.join("files", file_path)
+    counted_lines_amount =  count_lines_in_file(file_path, buffer=buffer) if buffer is not None else count_lines_in_file(file_path)
+    assert counted_lines_amount == lines_amount
